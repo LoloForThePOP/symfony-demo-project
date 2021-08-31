39882aac93b75c77c6f7a78f06d3f1a70565d8ca
LoloForThePOP <57801012+LoloForThePOP@users.noreply.github.com>
2021-08-31 11:06:14 +0200

    maker and annotations bundle

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    /**
+     * @Route("/", name="demo")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DemoController.php',
+        ]);
+    }
+}
